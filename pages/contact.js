c531407753d08bdbf611c4942b6f46ad46daeeca
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-05 15:29:42 -0500

    commit inicial

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import MainLayout from "../components/layout/MainLayout";
+
+export default function ContactPage() {
+  return (
+    <MainLayout>
+        <h1 className={'title'}>Contact Page</h1>
+
+        <p className={'description'}>
+          Get started by editing <code className={'code'}>pages/contact.js</code>
+        </p>
+      </MainLayout>
+  )
+}
