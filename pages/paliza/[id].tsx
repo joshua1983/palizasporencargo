61c7845dc30269fc703ef4478b0ed5fbbd432505
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-09 20:45:10 -0500

    link

--- pages/paliza/[id].tsx
+++ pages/paliza/[id].tsx
@@ -1,5 +1,6 @@
 import { Card, Grid, Text } from "@nextui-org/react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 import { peopleApi } from "../../api";
 import { Paliza, PalizaListResponse } from "../../components/interfaces";
 import MainLayout from "../../components/layouts/MainLayout";
@@ -9,33 +10,26 @@ interface Props {
 }
 
 const PalizaPage: NextPage<Props> = ({ paliza }) => {
+  const router = useRouter();
+  console.log(router.asPath);
   return (
     <MainLayout title="Toma tu paliza">
-          <h1>{paliza.nombre}</h1>
-
-          <Grid.Container css={{marginTop:'5px'}}>
-              <Grid xs={12} >
-                  <Card isHoverable css={{ padding: '30px' }}>
-                      <Card.Body>
-                          <Card.Image src={'https://i.gifer.com/177l.gif'} alt={paliza.nombre} width="100%" height={140} />
-                      </Card.Body>
-                  </Card>
-              </Grid>
-              <Grid xs={12} sm={8}>
-                  <Card>
-                      <Card.Header css={{display:'flex', justifyContent:'space-between'}}>
-                          {/* <Text h1 transform="capitalize">{paliza.nombre}</Text> */}
-                      </Card.Header>
-                      <Card.Body>
-                          <Text size={30}>Haz recibido una paliza</Text>
-                         
-                      </Card.Body>
-                  </Card>
-              </Grid>
-          </Grid.Container>
+      <h1>{paliza.nombre}</h1>
+      <h3>Haz recibido una paliza</h3>
+      <Grid.Container css={{ marginTop: "5px" }} justify="center">
+        <Grid xs={12}>
+          <Card isHoverable css={{ padding: "30px" }}>
+            <Card.Body>
+              <Card.Image src={"https://i.gifer.com/177l.gif"} alt={paliza.nombre} width="100%" height={140} />
+              <Text>Envia tu paliza con el siguiente link: {window.location.href}</Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </MainLayout>
   );
 };
+
 
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
@@ -52,18 +46,18 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { id } = ctx.params as { id: string };
 
-  const { data } = await peopleApi.get<PalizaListResponse>('/data.json');
+  const { data } = await peopleApi.get<PalizaListResponse>("/data.json");
   const peoples: Paliza[] = data.usuarios?.map((people, index) => ({
     ...people,
     id: people.id,
     img: `https://raw.githubusercontent.com/joshua1983/palizasporencargo/main/public/people/${people.imgName}`,
   }));
 
-  const people = peoples.filter(p => p.id.toString() === id);
+  const people = peoples.filter((p) => p.id.toString() === id);
 
   return {
     props: {
-      paliza: people[0]
+      paliza: people[0],
     },
   };
 };
