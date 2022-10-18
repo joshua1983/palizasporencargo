8edc9834a40c97d5625ef5e17e07fdeb1b2bd11f
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-18 11:42:07 -0500

    random gif with axios

--- api/gifApi.ts
+++ api/gifApi.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const gifSearch = async (query: string) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=7zWpG8lu3iTSGQJSCzetjxW6ANLuTDGg&limit=20&q=${query}`;
-  const resp = await fetch(url, { method: "GET" });
-    return await resp.json();
+  return axios.get(url).then( res => res.data)
 };
 
 export default gifSearch;
