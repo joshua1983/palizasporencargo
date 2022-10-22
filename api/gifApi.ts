a753925cf2ef28c7b4ff03e78087cb6fe4b17d85
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-22 08:32:31 -0500

    add environment variables

--- api/gifApi.ts
+++ api/gifApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const gifSearch = async (query: string) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=7zWpG8lu3iTSGQJSCzetjxW6ANLuTDGg&limit=20&q=${query}`;
+  const api_key = process.env.GIPHY_API_KEY;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&limit=20&q=${query}`;
   return axios.get(url).then( res => res.data)
 };
 
