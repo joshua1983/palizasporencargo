4df13a6f59f7be92f75b5f43165624e4a656afba
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-22 09:15:34 -0500

    Dinamic menu

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -3,9 +3,13 @@ import React, { CSSProperties } from "react";
 import { ActiveLink } from "./ActiveLink";
 import styles from "./Navbar.module.css";
 import { Text, useTheme } from "@nextui-org/react";
+import { useRouter } from "next/router";
+import logoNextJs from "../../public/next.png";
+import Image from "next/image";
 
 export const Navbar = () => {
-  const { theme } = useTheme();
+  const router = useRouter();
+  const pathName = router.pathname;
 
   const menuItems = [
     {
@@ -23,13 +27,28 @@ export const Navbar = () => {
     padding: "1px 20px",
   };
 
-  return (
-    <div style={styleMenu}>
+  let menuNav;
+  if (pathName === "/") {
+    menuNav = (
+      <nav className={styles["menu-container"]}>
+        <Image src={logoNextJs} alt="NextJS" width={103} height={62}></Image>
+        <Text>
+          Codigo fuente: <a href="https://github.com/joshua1983/palizasporencargo">GitHub</a>
+        </Text>
+      </nav>
+    );
+  } else {
+    menuNav = (
       <nav className={styles["menu-container"]}>
         {menuItems.map(({ text, href }) => (
           <ActiveLink key={href} text={text} href={href}></ActiveLink>
         ))}
       </nav>
+    );
+  }
+  return (
+    <div style={styleMenu}>
+      {menuNav}
     </div>
   );
 };
