7647cfecb07aa9d8f8c092bc5966a120fc07be5b
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2022-10-29 23:14:25 -0500

    resolve server url

--- api/peopleApi.ts
+++ api/peopleApi.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { Paliza } from "../components/interfaces";
 
 export const peopleApi = (endpoint: string) => {
-  const SERVER_URL = process.env.SERVER_URL as string;
+  const SERVER_URL = process.env.VERCEL_URL as string;
   return fetch(`${SERVER_URL}/api${endpoint}`).then((res: any) => res.json());
 };
 
 export const savePaliza = async (data: Paliza) => {
-  const SERVER_URL = process.env.SERVER_URL as string;
+  const SERVER_URL = process.env.VERCEL_URL as string;
   return await axios.post(`${SERVER_URL}/api/paliza`, data).then((res: any) => res.data);
 };
--- pages/api/paliza.ts
+++ pages/api/paliza.ts
@@ -1,6 +1,5 @@
 import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
-import { Paliza } from "../../components/interfaces";
 import { connectToDatabase, BD_NAME_PALIZAS } from "../../lib/mongodb";
 
 type ApiData = {
